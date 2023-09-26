e5bd62cfc8029921c5f833b8684942831a6946b1
citrinemoons <129808315+citrinemoons@users.noreply.github.com>
2023-09-26 11:20:31 +1000

    drag script implemented
    - taken my old drag script from secrets and added more grocery "bundles" to add more variety for players

---
+++ Assets/Scripts/Drag.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class Drag : MonoBehaviour
+{
+    public delegate void DragEndedDelegate(Drag draggableObject);
+    public DragEndedDelegate dragEndedCallback;
+
+
+    Vector3 mousePosition;
+    private bool isDragged;
+    private Vector3 getMousePos()
+    {
+        return Camera.main.WorldToScreenPoint(transform.position);
+    }
+
+    private void OnMouseDown()
+    {
+        mousePosition = Input.mousePosition - getMousePos();
+    }
+
+    private void OnMouseUp()
+    {
+        isDragged = false;
+        //dragEndedCallback(this);
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition - mousePosition);
+    }
+
+
+}
+
+// thank you to curiousbits on youtube for the tutorial to build this script :) 
+// https://youtu.be/axW46wCJxZ0 
